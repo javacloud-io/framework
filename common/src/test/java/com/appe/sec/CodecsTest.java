a407d6f6825913c2b047dbf621c67b9e55fc5d07
ho <huonghovan@yahoo.com>
2015-03-02 23:01:55 -0800

    clean up

--- common/src/test/java/com/appe/sec/CodecsTest.java
+++ common/src/test/java/com/appe/sec/CodecsTest.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.appe.util;
+package com.appe.sec;
 import java.util.UUID;
 
 import com.appe.sec.Codecs;
