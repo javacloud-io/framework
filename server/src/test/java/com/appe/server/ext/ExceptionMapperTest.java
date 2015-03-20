b3c0b56e74054126f131dcda7001b4fe138275c8
ho <huonghovan@yahoo.com>
2015-03-19 21:53:45 -0700

    test ex

--- server/src/test/java/com/appe/server/ext/ExceptionMapperTest.java
+++ server/src/test/java/com/appe/server/ext/ExceptionMapperTest.java
@@ -31,7 +31,7 @@ public class ExceptionMapperTest {
 	
 	@Test
 	public void testEx() {
-		Response rsp = mapper.toResponse(new Exception("test unknown exception"));
+		Response rsp = mapper.toResponse(new Exception("it's a test exception"));
 		Assert.assertEquals(500, rsp.getStatus());
 	}
 }
