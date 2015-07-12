1ce417d4dfdf89440a675ecbbfe9df9a8497d51a
ho <huonghovan@yahoo.com>
2015-07-12 08:49:28 -0700

    rename package again

--- common-ext/src/main/java/com/appe/ext/Namespaceable.java
+++ common-ext/src/main/java/com/appe/ext/Namespaceable.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.appe.authz;
+package com.appe.ext;
 
 import java.security.Principal;
 
