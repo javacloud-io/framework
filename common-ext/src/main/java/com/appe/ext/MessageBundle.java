fbc21bf00b6a40ab968948dfedd77238f27551b6
ho <huonghovan@yahoo.com>
2015-07-14 08:46:14 -0700

    move back MessageBundle to ext

--- common-ext/src/main/java/com/appe/ext/MessageBundle.java
+++ common-ext/src/main/java/com/appe/ext/MessageBundle.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.appe.ext.internal;
+package com.appe.ext;
 
 import com.appe.registry.AppeConfig;
 /**
