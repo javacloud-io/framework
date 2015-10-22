c50a269574ee4d1ed98e42e80bb4416a1d0d61a7
ho <huonghovan@yahoo.com>
2015-10-21 22:30:09 -0700

    remove mapping

--- server/src/main/java/com/appe/server/internal/JspViewRender.java
+++ server/src/main/java/com/appe/server/internal/JspViewRender.java
@@ -1,8 +1,6 @@
 package com.appe.server.internal;
 
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -12,35 +10,12 @@ import javax.servlet.http.HttpServletResponse;
 import com.appe.util.Objects;
 /**
  * Map a given servlet to a JSP page, by default the page will be located under WEB-INF
- * <init-param>
- *     <param-name>mapping</param-name>
- *	   <param-value></param-value>
- * </init-param>
  *		
  * @author ho
  *
  */
 public class JspViewRender extends HttpServlet {
 	private static final long serialVersionUID = 1336457408255780274L;
-	
-	//keep a map of request URI to views if any specified
-	private static final Map<String, String> mapper = new HashMap<>();
-	
-	@Override
-	public void init() throws ServletException {
-		super.init();
-		
-		//requestURI:jsp,requestURI:jsp
-		String mapping = getInitParameter("mapping");
-		if(mapping != null && !mapping.isEmpty()) {
-			for(String mapp: Objects.toArray(mapping, ",", true)) {
-				String[] pair = Objects.toArray(mapp, ":", true);
-				if(pair.length == 2) {
-					mapper.put(pair[0], pair[1]);
-				}
-			}
-		}
-	}
 
 	/**
 	 * Forward to the correct view if specified
@@ -61,12 +36,8 @@ public class JspViewRender extends HttpServlet {
 	protected String resolveView(HttpServletRequest req) throws ServletException {
 		String path = req.getPathInfo();
 		String requestURI = Objects.isEmpty(path)? req.getServletPath() : (req.getServletPath() + path);
-		String tview = mapper.get(requestURI);
 		
-		//ASSUMING JSP VIEW IF NOT FOUND
-		if(tview == null) {
-			tview = "/WEB-INF" + requestURI + ".jsp";
-		}
-		return tview;
+		//ASSUMING JSP VIEW
+		return "/WEB-INF" + requestURI + ".jsp";
 	}
 }
