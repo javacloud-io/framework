c2fc8e88294725c3d6f44b5ee5efbe383733f89c
ho <huonghovan@yahoo.com>
2018-10-14 08:38:57 -0700

    clean up

--- txn/src/main/java/io/javacloud/framework/tx/Propagation.java
+++ txn/src/main/java/io/javacloud/framework/tx/Propagation.java
@@ -7,29 +7,24 @@ package io.javacloud.framework.tx;
  *
  */
 public enum Propagation {
-	//If a client invokes the enterprise bean's method while the client is associated with a transaction context, the container invokes the enterprise bean's method in the client's transaction context.
 	//If there is no existing transaction, an exception is thrown.
 	MANDATORY,
 	
-	//If a client invokes the enterprise bean's method while the client is associated with a transaction context, the container invokes the enterprise bean's method in the client's transaction context.
-	//If the client invokes the enterprise bean's method while the client is not associated with a transaction context, the container automatically starts a new transaction before delegating a method call to the enterprise bean method.
+	//If there is associated with a transaction context, the container invokes the enterprise bean's method in the client's transaction context.
+	//If there is not associated with a transaction context, the container automatically starts a new transaction before delegating a method call to the enterprise bean method.
 	REQUIRED,
-		
-	//The container must invoke an enterprise bean method whose transaction attribute is set to REQUIRES_NEW with a new transaction context.
+	
 	//If the client invokes the enterprise bean's method while the client is not associated with a transaction context, the container automatically starts a new transaction before delegating a method call to the enterprise bean business method.
 	//If a client calls with a transaction context, the container suspends the association of the transaction context with the current thread before starting the new transaction and invoking the method. The container resumes the suspended transaction association after the method and the new transaction have been completed.
 	REQUIRES_NEW,
 		
-	//If a client invokes the enterprise bean's method while the client is associated with a transaction context, the container invokes the enterprise bean's method in the client's transaction context.
-	//If there is no existing transaction, an exception is thrown.
+	//The client is required to call without a transaction context, otherwise an exception is thrown.
 	NEVER,
 	
 	//If the client calls with a transaction context, the container performs the same steps as described in the REQUIRED case.
 	//If the client calls without a transaction context, the container performs the same steps as described in the NOT_SUPPORTED case.
-	//The SUPPORTS transaction attribute must be used with caution. This is because of the different transactional semantics provided by the two possible modes of execution. Only enterprise beans that will execute correctly in both modes should use the SUPPORTS transaction attribute.
 	SUPPORTS,
 	
-	//The container invokes an enterprise bean method whose transaction attribute NOT_SUPPORTED with an unspecified transaction context.
 	//If a client calls with a transaction context, the container suspends the association of the transaction context with the current thread before invoking the enterprise bean's business method. The container resumes the suspended association when the business method has completed.
 	NOT_SUPPORTED,
 }
