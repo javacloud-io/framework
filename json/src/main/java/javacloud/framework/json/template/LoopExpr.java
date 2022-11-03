ce9d5b2f75682c0bb97495f338a61f7c84cfdde8
Ho <ho@jecdis.com>
2022-11-03 14:26:40 -0700

    refactor loop expression

---
+++ json/src/main/java/javacloud/framework/json/template/LoopExpr.java
@@ -0,0 +1,35 @@
+package javacloud.framework.json.template;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.JsonNodeFactory;
+
+/**
+ * loop through input using expression and child template
+ */
+public class LoopExpr implements JsonExpr {
+	private final JsonExpr expr;
+	private final JsonTemplate template;
+	
+	public LoopExpr(JsonExpr expr, JsonTemplate template) {
+		this.expr = expr;
+		this.template = template;
+	}
+	
+	@Override
+	public JsonNode apply(JsonNode input) {
+		JsonNode context = expr.apply(input);
+		if (context.isArray()) {
+			ArrayNode out = JsonNodeFactory.instance.arrayNode();
+			context.forEach(n -> {
+				// using child node
+				JsonNode o = template.apply(n);
+				if (!JsonPath.isNullOrMissing(o)) {
+					out.add(o);
+				}
+			});
+			return out;
+		}
+		return template.apply(context);
+	}
+}
