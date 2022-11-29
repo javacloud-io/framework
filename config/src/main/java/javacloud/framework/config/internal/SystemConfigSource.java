6df49c4be98455e4b1aba974fd2abdcdd43afcb7
Ho <ho@jecdis.com>
2022-11-28 17:06:21 -0800

    allows config file

--- config/src/main/java/javacloud/framework/config/internal/SystemConfigSource.java
+++ config/src/main/java/javacloud/framework/config/internal/SystemConfigSource.java
@@ -1,17 +1,19 @@
 package javacloud.framework.config.internal;
 
+import java.io.File;
+import java.util.Properties;
+
 import javacloud.framework.util.Converters;
 /**
  * Use in replace of System.getProperties() to inject runtime properties
- * 
- * @author tobiho
  *
  */
 public final class SystemConfigSource extends StandardConfigSource {
+	private static final String CONFIG_FILE = "javacloud.config.file";
 	private static final SystemConfigSource SOURCE = new SystemConfigSource();
 	
 	SystemConfigSource() {
-		super(System.getProperties());
+		super(systemProperties());
 	}
 	
 	public static SystemConfigSource get() {
@@ -24,5 +26,20 @@ public final class SystemConfigSource extends StandardConfigSource {
 	
 	public void setPropertyIfAbsent(String name, Object value) {
 		properties.putIfAbsent(name, Converters.toString(value));
+	}
+	
+	/**
+	 * 
+	 * @return merge of file and system properties
+	 */
+	static Properties systemProperties() {
+		Properties properties = new Properties();
+		String configFile = System.getProperty(CONFIG_FILE, System.getenv(CONFIG_FILE));
+		if (configFile != null) {
+			properties.putAll(loadProperties(new File(configFile)));
+		}
+		properties.putAll(System.getProperties());
+		properties.remove(CONFIG_FILE);
+		return properties;
 	}
 }
